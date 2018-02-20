aa7a76777727882cc214f0d464769038ee65402f
ZMH <zinminht3t@gmail.com>
2018-02-21 00:02:07 +0800

    Workshop 3.2

---
+++ Workshop3b/BankAccount.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop3b
+{
+    class Account
+    {
+        string accountNumber;
+        string accountHolderName;
+        double balance;
+        public Account(string accountNumber, string accountHolderName, double balance)
+        {
+            this.accountNumber = accountNumber;
+            this.accountHolderName = accountHolderName;
+            this.balance = balance;
+        }
+        public Account(string accountNumber, Customer customer, double balance): this(accountNumber, customer.Name, balance)
+        {
+
+        }
+        public string AccountNumber
+        {
+            get { return accountNumber; }
+            set { accountNumber = value; }
+        }
+        public string AccountHolderName
+        {
+            get { return accountHolderName; }
+            set { accountHolderName = value; }
+        }
+        public double Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+        public void Withdraw(double amount)
+        {
+            if (amount <= balance)
+            {
+                balance -= amount;
+            }
+        }
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+        public void TransferTo(double amount, Account account)
+        {
+            Withdraw(amount);
+            account.Deposit(amount);
+        }
+        public string Show()
+        {
+            return string.Format(" Account No : {0} \n Holder Name : {1} \n Balance : {2} \n **************************", accountNumber, accountHolderName, balance);
+        }
+    }
+}
---
+++ Workshop3b/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop3b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer y = new Customer("Tan Ah Kow", "20, Seaside Road", "XXX20", new DateTime(1989, 10, 11));
+            Customer z = new Customer("Kim Lee Keng", "2, Rich View", "XXX9F", new DateTime(1993, 4, 25));
+            Account a = new Account("001-001-001", y, 2000);
+            Account b = new Account("001-001-002", z, 5000);
+            Console.WriteLine(a.Show());
+            Console.WriteLine(b.Show());
+            a.Deposit(100);
+            Console.WriteLine(a.Show());
+            a.Withdraw(200);
+            Console.WriteLine(a.Show());
+            a.TransferTo(300, b);
+            Console.WriteLine(a.Show());
+            Console.WriteLine(b.Show());
+        }
+    }
+}
