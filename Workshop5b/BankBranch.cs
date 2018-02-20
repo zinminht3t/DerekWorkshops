fde75517e22042a5a4421da21a72df1c7fb972c8
ZMH <zinminht3t@gmail.com>
2018-02-21 00:38:28 +0800

    Workshop 5.2

---
+++ Workshop5b/BankBranch.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop5b
+{
+    class BankBranch
+    {
+        string BranchName;
+        string BranchManager;
+        List<Account> accountLists;
+
+        public void AddAcount(Account account)
+        {
+            accountLists.Add(account);
+        }
+        public void PrintCustomers()
+        {
+            for (int i = 0; i < accountLists.Count; i++)
+            {
+                Console.WriteLine(accountLists[i].AccountHolderName);
+                Console.WriteLine("------");
+            }
+            Console.WriteLine();
+        }
+
+        public double TotalDeposits()
+        {
+            double total = 0;
+            for (int i = 0; i < accountLists.Count; i++)
+            {
+                total += accountLists[i].Balance;
+            }
+            return total;
+        }
+        public double TotalInterestPaid()
+        {
+            double total = 0;
+            for (int i = 0; i < accountLists.Count; i++)
+            {
+                if (accountLists[i].CalculateInterest() > 0)
+                    total += accountLists[i].CalculateInterest();
+            }
+            return total;
+
+        }
+        public double TotalInterestEarned()
+        {
+            double total = 0;
+            for (int i = 0; i < accountLists.Count; i++)
+            {
+                if (accountLists[i].CalculateInterest() < 0)
+                    total -= accountLists[i].CalculateInterest();
+            }
+            return total;
+
+        }
+    }
+}
