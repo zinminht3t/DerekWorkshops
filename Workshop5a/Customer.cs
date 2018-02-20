a68231483187712f3165d3313b24b3a0a974959f
ZMH <zinminht3t@gmail.com>
2018-02-21 00:24:03 +0800

    Workshop 5a Polymorphism

---
+++ Workshop5a/Customer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop5a
+{
+    class Customer
+    {
+        string name;
+        string address;
+        string passport;
+        DateTime dateofbirth;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        public string Passport
+        {
+            get { return passport; }
+            set { passport = value; }
+        }
+        public DateTime DateOfBirth
+        {
+            get { return dateofbirth; }
+            set { dateofbirth = value; }
+        }
+        public Customer(string name, string address, string passport, DateTime dateofbirth)
+        {
+            this.name = name;
+            this.address = address;
+            this.passport = passport;
+            this.dateofbirth = dateofbirth;
+        }
+        public int GetAge()
+        {
+            return DateTime.Today.Year - dateofbirth.Year;
+        }
+
+    }
+}
