c7c89372c31140dd1ea6d31530abdf17d186bec5
ZMH <zinminht3t@gmail.com>
2018-02-20 23:50:00 +0800

    Workshop 3.1

--- Workshop3/Program.cs
+++ Workshop3/Program.cs
@@ -10,6 +10,17 @@ namespace Workshop3
     {
         static void Main(string[] args)
         {
+            Account a = new Account("001-001-001", "Tan Ah Kow", 2000);
+            Account b = new Account("001-001-002", "Kim Keng Lee", 5000);
+            Console.WriteLine(a.Show());
+            Console.WriteLine(b.Show());
+            a.Deposit(100);
+            Console.WriteLine(a.Show());
+            a.Withdraw(200);
+            Console.WriteLine(a.Show());
+            a.TransferTo(300, b);
+            Console.WriteLine(a.Show());
+            Console.WriteLine(b.Show());
         }
     }
 }
